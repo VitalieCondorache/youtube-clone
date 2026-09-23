e600e83587101e644f1cb58b73ad8ae108b55a97
Vitalyus <vitalie.condor@gmail.com>
2026-09-23 11:10:46 +0300

    chore: initialize project structure and setup backend boilerplate with Express and MongoDB

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('src/uploads'));
+
+// Base Route
+app.get('/api/v1/health', (req, res) => {
+    res.status(200).json({ status: 'success', message: 'YouTube Clone API is running' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running in development mode on port ${PORT}`);
+});
